ea92521f7300719ab7e100fada62f43cf23573c2
Your Name <you@example.com>
2021-02-19 11:07:35 +0100

    initial data

--- fetch.sh
+++ fetch.sh
@@ -1,9 +1,16 @@
+#!/bin/bash
+
+source env/bin/activate
+
 LAYERS=`flowmaps-data population list | sed 1d`
+FORMAT=parquet
+
 echo $LAYERS
 
 for layer in $LAYERS; do
 	echo $layer
-	FILENAME=data/$layer/$layer.population.parquet
-	flowmaps-data population download --layer $layer --output-format parquet --output-file $FILENAME
+	FILENAME=data/$layer/$layer.population.$FORMAT
+	mkdir -p data/$layer
+	flowmaps-data population download --layer $layer --output-format $FORMAT --output-file $FILENAME
 done
 
